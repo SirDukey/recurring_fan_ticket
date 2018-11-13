acfc1d458875f87266dac6e99a88f222de3ecab3
Michael Duke <42153017+SirDukey@users.noreply.github.com>
2018-11-13 09:00:25 +0200

    Create aircon_check.py

---
+++ aircon_check.py
@@ -0,0 +1,17 @@
+
+import smtplib
+
+def send_mail():
+    log_msg = 'Please check the aircon filters in the svr room'
+    mail_srv = 'mail.novusgroup.co.za'
+    mail_port = '25'
+    mail_name = 'itmonitor@novusgroup.co.za'
+    mail_recip = ['helpdesk@novusgroup.co.za']
+    mail_sub = 'STT server fan checkup'
+    mail_msg = 'Subject: {0}\n\n{1}'.format(mail_sub, log_msg)
+    smtpServ = smtplib.SMTP(mail_srv, mail_port)
+    smtpServ.sendmail(mail_name, mail_recip, mail_msg)
+    smtpServ.quit()
+    
+if __name__ == '__main__':
+send_mail()
