f642cdff7e9ccb9ce809e71b42f7de48b280afc5
Michael Duke <42153017+SirDukey@users.noreply.github.com>
2019-01-10 10:54:29 +0200

    Create recording_archive.py

---
+++ recording_archive.py
@@ -0,0 +1,17 @@
+
+import smtplib
+
+def send_mail():
+    log_msg = 'Please archive the articles folder on the recording system'
+    mail_srv = 'mail.novusgroup.co.za'
+    mail_port = '25'
+    mail_name = 'itmonitor@novusgroup.co.za'
+    mail_recip = ['helpdesk@novusgroup.co.za']
+    mail_sub = 'Recording articles archive'
+    mail_msg = 'Subject: {0}\n\n{1}'.format(mail_sub, log_msg)
+    smtpServ = smtplib.SMTP(mail_srv, mail_port)
+    smtpServ.sendmail(mail_name, mail_recip, mail_msg)
+    smtpServ.quit()
+    
+if __name__ == '__main__':
+send_mail()
